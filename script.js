4b2efd93c20cdd3c5e89c8b03f8f920a5e641601
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-10 22:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get(name){
+		return this.name
+	}
+	set(name){
+		return this.name
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age)
+	}
+	study(){
+		return this.name + "is studying"
+	}
+}
 
-class Teacher extends Person {}
-
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age)
+	}
+	teach(){
+		return this.name + "is teaching"
+	}
+}
+const person = new Person("John", 25);
+console.log(person.name);
+person.age = 30;
+console.log(person.age); 
+const student = new Student("Alice", 22);
+student.study();
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
